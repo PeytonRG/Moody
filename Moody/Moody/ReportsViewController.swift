78750dfbbd930aa6b216d33e0a6ba557bf2859d6
Peyton Gasink <peytongasink@att.net>
2020-08-01 16:30:58 -0500

    the result of the incredibly horrible process of installing and using Charts

--- Moody/Moody/ReportsViewController.swift
+++ Moody/Moody/ReportsViewController.swift
@@ -7,14 +7,86 @@
 //
 
 import UIKit
+import CoreData
+import Charts
 
 class ReportsViewController: UIViewController {
-
+    
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    var moodRecords: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let unitsSold = [10.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        GetCoreDataRecords()
+        //        setChart(dataPoints: Array(activityTypes.values), values: unitsSold)
     }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        var dataEntries: [ChartDataEntry] = []
+        for i in 0..<dataPoints.count {
+            let dataEntry1 = PieChartDataEntry(value: values[i], label: dataPoints[i], data:  dataPoints[i] as AnyObject)
+            dataEntries.append(dataEntry1)
+        }
 
-
+        let pieChartDataSet = PieChartDataSet(entries: dataEntries, label: "Activities")
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        pieChartDataSet.colors = colors
+    }
+    
+    func GetCoreDataRecords() {
+        //        Get mood records from core data
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "MoodRecord")
+        //        fetchRequest.predicate = NSPredicate(format: "moodID = %ld", 5)
+        
+        do {
+            moodRecords = try managedContext.fetch(fetchRequest)
+            var values: [Int] = []
+            for record in moodRecords {
+                let activityID = record.value(forKeyPath: "activityID") as? Int
+                values.append(activityID!)
+            }
+            
+            let mappedItems = values.map { ($0, 1) }
+            let dict = Dictionary(mappedItems, uniquingKeysWith: +)
+            var activityNames: [String] = []
+            var percentages: [Double] = []
+            
+            for pair in dict {
+                let activityCount = pair.value
+                let fraction = Double(activityCount) / Double(values.count)
+                percentages.append(fraction)
+                let activityName = activityTypes[pair.key]!
+                activityNames.append(activityName)
+            }
+            
+            setChart(dataPoints: activityNames, values: percentages)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
 }
-
