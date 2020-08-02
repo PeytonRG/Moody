d718b9d19d276ab332d1265d037a5673c4e5482e
Peyton Gasink <peytongasink@att.net>
2020-08-02 17:23:11 -0500

    added a tableviewcontroller for picking a mood to graph

---
+++ Moody/Moody/AnalyticsTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AnalyticsTableViewController.swift
+//  Moody
+//
+//  Created by Peyton Gasink on 8/2/20.
+//  Copyright © 2020 Peyton Gasink. All rights reserved.
+//
+
+import UIKit
+
+class AnalyticsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moodTypes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let mood = moodTypes[indexPath.row + 1]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnalyticsTableViewCell", for: indexPath)
+        
+        // Set the label text for the row
+        cell.textLabel!.text = mood
+        return cell
+    }
+}
